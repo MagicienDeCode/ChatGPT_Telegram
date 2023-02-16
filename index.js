3cb98b317652ab77f0ba06bf3ea69ffdcdc465af
Xiang LI <xli@slimpay.com>
2023-02-16 16:05:31 +0100

    init

---
+++ index.js
@@ -0,0 +1,149 @@
+require("dotenv").config();
+const { Configuration, OpenAIApi } = require("openai");
+const {
+  getImage,
+  getChat,
+
+  correctEngish,
+} = require("./Helper/functions");
+const User = require("./Model/user");
+
+const { Telegraf } = require("telegraf");
+const { default: axios } = require("axios");
+const logger = require("./Helper/logger");
+const connectDB = require("./Helper/db");
+const Group = require("./Model/groups");
+const checkAndSave = require("./Helper/saveToDb");
+
+const configuration = new Configuration({
+  apiKey: process.env.API,
+});
+
+const openai = new OpenAIApi(configuration);
+module.exports = openai;
+
+const bot = new Telegraf(process.env.TG_API);
+
+connectDB();
+
+// Bot on start
+bot.start(async(ctx) => {
+  if (ctx.chat.type === "group") {
+    logger.info(`Bot started In: ${ctx.chat.title} `);
+  } else if (ctx.chat.type === "private") {
+    logger.info(`Bot started By ${ctx.chat.username || ctx.chat.first_name} `);
+  }
+  await checkAndSave(ctx)
+
+  ctx.reply(
+    "Welcome To AI Bot 🧿 \n\nCommands 👾 \n/ask  ask anything from me \n/image to create image from text  \n/en to correct your grammer \n\n\nContract @Chetan_Baliyan if you want to report any BUG or change in features"
+  );
+});
+
+bot.help((ctx) => {
+  ctx.reply(
+    "\nCommands 👾 \n\n/ask  ask anything from me \n/image to create image from text  \n/en to correct your grammer \n\n\nContract @Chetan_Baliyan if you want to report any BUG or change in features "
+  );
+});
+
+//Bot on Image command
+bot.command("image", async (ctx) => {
+  const text = ctx.message.text?.replace("/image", "")?.trim().toLowerCase();
+  logger.info(`Image: ${ctx.from.username || ctx.from.first_name}: ${text}`);
+  if (text) {
+    const res = await getImage(text);
+
+    if (res) {
+      ctx.sendChatAction("upload_photo");
+
+      ctx.telegram.sendPhoto(ctx.message.chat.id, res, {
+        reply_to_message_id: ctx.message.message_id,
+      });
+    } else {
+      ctx.telegram.sendMessage(
+        ctx.message.chat.id,
+        "I can't generate image for this text",
+        {
+          reply_to_message_id: ctx.message.message_id,
+        }
+      );
+    }
+  } else {
+    ctx.telegram.sendMessage(
+      ctx.message.chat.id,
+      "You have to give some description after /image",
+      {
+        reply_to_message_id: ctx.message.message_id,
+      }
+    );
+  }
+  await checkAndSave(ctx);
+});
+
+//Bot on ask command
+
+bot.command("ask", async (ctx) => {
+  const text = ctx.message.text?.replace("/ask", "")?.trim().toLowerCase();
+
+  logger.info(`Chat: ${ctx.from.username || ctx.from.first_name}: ${text}`);
+
+  if (text) {
+    ctx.sendChatAction("typing");
+    const res = await getChat(text);
+    if (res) {
+      ctx.telegram.sendMessage(ctx.message.chat.id, res, {
+        reply_to_message_id: ctx.message.message_id,
+      });
+    }
+  } else {
+    ctx.telegram.sendMessage(
+      ctx.message.chat.id,
+      "Please ask anything after /ask",
+      {
+        reply_to_message_id: ctx.message.message_id,
+      }
+    );
+  }
+
+  await checkAndSave(ctx);
+});
+
+// Bot on en command
+bot.command("en", async (ctx) => {
+  const text = ctx.message.text?.replace("/en", "")?.trim().toLowerCase();
+
+  if (text) {
+    ctx.sendChatAction("typing");
+    const res = await correctEngish(text);
+    if (res) {
+      ctx.telegram.sendMessage(ctx.message.chat.id, res, {
+        reply_to_message_id: ctx.message.message_id,
+      });
+    }
+  } else {
+    ctx.telegram.sendMessage(
+      ctx.message.chat.id,
+      "Please ask anything after /en",
+      {
+        reply_to_message_id: ctx.message.message_id,
+      }
+    );
+  }
+});
+
+bot.command("yo", async (ctx) => {
+  const text = ctx.message.text?.replace("/yo", "")?.trim().toLowerCase();
+  logger.info(`Joke: ${ctx.from.username || ctx.from.first_name}: ${text}`);
+
+  const ress = await axios.get("https://api.yomomma.info/");
+
+  ctx.sendChatAction("typing");
+
+  if (ress.data?.joke) {
+    ctx.telegram.sendMessage(ctx.message.chat.id, ress.data?.joke, {
+      reply_to_message_id: ctx.message.message_id,
+    });
+  }
+});
+
+bot.launch();
